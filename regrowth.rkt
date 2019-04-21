4359ed5fd1347751e51cae9dde6c661e1a840640
pcb3 <petercraigblaas@gmail.com>
2019-04-21 15:27:49 +1200

    Define constants and structures

--- regrowth.rkt
+++ regrowth.rkt
@@ -2,3 +2,20 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname regrowth) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ; Regrowth: A simple forest-fire simulator
+
+(require 2htdp/universe)
+(require 2htdp/image)
+
+; physical constants
+(define RADIUS 5)
+(define SIZE 10)
+(define SCENE-SIZE (* SIZE SIZE))
+
+; graphical constants
+(define MT (empty-scene SCENE-SIZE SCENE-SIZE))
+(define (tree state) (circle RADIUS 'solid state))
+
+; structures
+(define-struct forest [tree fire wind rain])
+(define-struct tree [species height position burning])
+(define-struct species [totara rimu matai ponga]) 
